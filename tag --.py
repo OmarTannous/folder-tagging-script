6f558042e470da1a22c0a010494831f3599a2d43
OmarTannous <otannous@uoguelph.ca>
2019-03-18 17:24:00 -0400

    new file:   tag ++.py 	new file:   tag --.py

---
+++ tag --.py
@@ -0,0 +1,10 @@
+import os, sys
+
+print("drag the folder into the window and press enter")
+name = input()
+
+name = name.strip('\"')
+
+name = name[(name.rfind('\\') + 1):]
+
+os.rename(name, "-- " + name)
